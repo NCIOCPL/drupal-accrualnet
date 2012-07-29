ef4d0a107f072bf944772455cd1c5a21f404289f
ficcolm <webcommauto@users.noreply.github.com>
2012-07-29 22:55:24 +0000

    fixed a few errors. added list of members to left.

--- themes/accrualnet/node-templates/node--community.tpl.php
+++ themes/accrualnet/node-templates/node--community.tpl.php
@@ -5,7 +5,7 @@
  * and open the template in the editor.
  */
 
-$gid = $group_group['und'][0]['value'];
+
 //print kprint_r(get_defined_vars());
 /*
 $query = new EntityFieldQuery();
@@ -30,14 +30,15 @@ foreach (array_keys($result['og_membership']) as $convo) {
 print kprint_r($convos);
  * 
  */
-
-$query = db_query("SELECT etid FROM {og_membership} WHERE gid=".$gid." AND entity_type='node'");
+$group = og_context();
+$query = db_query("SELECT etid FROM {og_membership} WHERE gid=".$group->gid." AND entity_type='node'");
 $convos = array();
 foreach ($query as $result) {
     $convos[] = node_load($result->etid);
 }
 //print kprint_r($convos);
 ?>
+<div id="community">
 <?php if ($body) print $body[0]['safe_value']; ?>
 <?php
 $o = '';
@@ -56,9 +57,13 @@ foreach ($convos as $convo) {
     $o .= '</div>'; // Ends title
     
     $o .= '<div class="conversation-preview">';
+    $o .= _preview_snippet($convo->body, 2);
+    /*
     $bodyContent = $convo->body['und'][0]['value'];
     $bodyTilPeriod = substr($bodyContent, 0, strpos($bodyContent, '.')+1);
     $o .= $bodyTilPeriod;
+     * 
+     */
     $o .= '</div>';
     
     $o .= '<div class="conversation-starter">';
@@ -68,7 +73,7 @@ foreach ($convos as $convo) {
     $o .= '</a>';
     $o .= '</div>';
     
-    $comment = comment_get_thread($convo, COMMENT_MODE_FLAT, 1);
+    $comment = comment_get_thread($convo, COMMENT_MODE_THREADED, 1);
     if ($comment != NULL) {
         $comment = comment_load($comment[0]);
         $commenter = user_load($comment->uid);
@@ -84,7 +89,8 @@ foreach ($convos as $convo) {
     $o .= '<div class="conversation-recent-comment-text">';
     $o .= '<a href="'.url('user/' . $commenter->uid).'">'.$commenter->name.'</a>';
     $o .= '<br/>';
-    $o .= $comment->comment_body['und'][0]['safe_value'];
+    $o .= _preview_snippet($comment->comment_body, 0, 200);
+    //$o .= $comment->comment_body['und'][0]['safe_value'];
     $o .= '</div>';
     $o .= '</div>';
     $o .= '</div>';
@@ -92,5 +98,54 @@ foreach ($convos as $convo) {
     $o .= '</div>'; // Ends paritcular convo
 }
 
+
 print $o;
 ?>
+</div>
+<?php
+drupal_add_js("(function ($) {
+        $(document).ready(function() {
+            $('nav#block-an-navigation-left-nav').append($('div#community-members'));
+        
+        });
+    }) (jQuery);", 'inline');
+?>
+
+    <?php
+    //$query = db_query("SELECT etid FROM {og_membership} WHERE gid=".$group->gid." AND entity_type='user'");
+    $query = new EntityFieldQuery();
+    $query->entityCondition('entity_type', 'og_membership')
+            ->propertyCondition('gid', $group->gid)
+            ->propertyCondition('entity_type', 'user', '=')
+            ->propertyCondition('state', '1')
+            ->pager(10);
+    $results = $query->execute();
+    if (count($results) > 0 && count($results['og_membership']) > 0): ?>
+<div id="community-members">
+    <div id="community-members-title">
+        Community Members
+    </div>
+    <div id="community-members-list">
+    <?php    
+    $ogmemberships = entity_load('og_membership', array_keys($results['og_membership']));
+    foreach ($ogmemberships as $ogm):
+        $user = user_load($ogm->etid);
+    ?>
+        <div class="community-member">
+            <span class="community-member-pic">
+                
+            </span>
+            <span class="community-member-name">
+                <a href="<?php print url('user/' . $user->uid); ?>"><?php print $user->name; ?></a>
+            </span>
+        </div>
+       <?php 
+    endforeach; ?>
+        <div id="community-members-pager">
+            <?php print theme('pager'); ?>
+        </div>
+        </div>
+    <?php endif;
+    ?>
+    
+</div>
