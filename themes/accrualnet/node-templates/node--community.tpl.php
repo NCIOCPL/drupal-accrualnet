c3c3cbc977f2c431c95ec090eb53d4273bcd42e5
doylejd <webcommauto@users.noreply.github.com>
2012-08-22 20:53:06 +0000

    Many updates to CSS for communities.

--- themes/accrualnet/node-templates/node--community.tpl.php
+++ themes/accrualnet/node-templates/node--community.tpl.php
@@ -8,20 +8,18 @@
 global $user;
 $o = '';
 $group = og_context();
-$query = db_query("SELECT etid FROM {og_membership} WHERE gid=".$group->gid." AND entity_type='node'");
-$convos = array();
-foreach ($query as $result) {
-    $convos[] = $result->etid;//node_load($result->etid);
-}
 
-$equery = new EntityFieldQuery();
-$equery->entityCondition('entity_type', 'node')
-            ->entityCondition('bundle', 'conversation')
-            ->propertyCondition('nid', $convos, 'IN')
-            ->propertyOrderBy('changed', 'DESC')
-            ->propertyCondition('status', 1);
-
-$nodes = _an_lifecycle_load_related_nodes($equery);
+$query = db_select('node', 'n')->fields('n', array('nid', 'title', 'changed'));
+$query->leftJoin('og_membership', 'ogm', 'ogm.etid=n.nid');
+$query->innerJoin('node_comment_statistics', 'c', 'n.nid=c.nid');
+$query->condition('n.type', 'conversation')
+        ->condition('n.status', '1')
+        ->condition('ogm.gid', $group->gid);
+$query->orderBy('c.last_comment_timestamp', 'DESC');
+$query = $query->extend('PagerDefault')->limit(10);
+$convos = $query->execute()->fetchAllAssoc('nid');
+$convosUnloaded = array_values($convos);
+ 
 //print kprint_r($convos);
 ?>
 <div id="community">
@@ -31,31 +29,44 @@ $nodes = _an_lifecycle_load_related_nodes($equery);
 
 <?php
 $o .= '';
-foreach ($nodes['nodes'] as $convo) {
+$o .= '<div class="community-buttons">';
+$o .= '<div class="start-a-convo"><span class="button"><a href="/node/add/conversation" title="Start a Conversation">Start a Conversation</a></span></div>  ';
+$userGroupRoles = og_get_user_roles($group->gid, $user->uid); 
+if(array_key_exists(3, $userGroupRoles) || in_array('administrator', $user->roles) || in_array('accrualnet_staff', $user->roles) ){
+    $o .= '<div class="manage-members">';
+    $o .= '<span class="button"><a href="/group/node/'.$group->etid.'/admin/people">Manage Group Members</a></span>';
+    $o .= '</div>';
+}
+$o .= '</div>';
+$o .= '<h2>Conversations</h2>';       
+foreach ($convos as $convoUnloaded) {
+    $convo = node_load($convoUnloaded->nid);
     $o .= '<div class="conversation-result';
-    if ($convos[count($convos) - 1] == $convo) $o .= ' last-result';
+    if ($convosUnloaded[count($convos) - 1] == $convoUnloaded) $o .= ' last-result';
+
     $o .= '">'; // Start Result
     $o .= '<div class="conversation-title">';
     $o .= '<a href="' . url('node/' . $convo->nid) . '">';
-	//$output .= $node->title;
+        //$output .= $node->title;
     $o .= check_plain($convo->title);
-	//$output .= filter_xss($node->title);
+        //$output .= filter_xss($node->title);
     $o .= '</a>';
     $o .= '<span class="conversation-last-updated">';
     $o .= 'Last Updated: ';
     $o .= date('M j, Y', $convo->changed);
     $o .= '</span>';
     $o .= '</div>'; // Ends title
-    
+
     $o .= '<div class="conversation-preview">';
     $o .= _preview_snippet($convo->body);
     $o .= '</div>';
-    
+
     $o .= _starter_snippet($convo->uid);
-    
+
     $o  .= _get_latest_comment($convo);
-    
+
     $o .= '</div>'; // Ends paritcular convo
+    
 }
 
 
@@ -104,14 +115,13 @@ drupal_add_js("(function ($) {
         <div id="community-members-pager">
             <?php print theme('pager'); ?>
         </div>
-<?php $userGroupRoles = og_get_user_roles($group->gid, $user->uid);?> 
-        <?php if(array_key_exists(3, $userGroupRoles)):?>
-        <div class="manage-members">
-            <span class="button"><a href="http://localhost/group/node/<?php print $group->etid;?>/admin/people">Manage Group Members</a></span>
-        </div>
+<?php $userGroupRoles = og_get_user_roles($group->gid, $user->uid); 
+        if(array_key_exists(3, $userGroupRoles) || in_array('administrator', $user->roles) || in_array('accrualnet_staff', $user->roles) ): ?>
+            <div class="manage-members">
+            <span class="button"><a href="/group/node/<?php print $group->etid; ?>/admin/people">Manage Group Members</a></span>
+            </div>
         <?php endif;?>
         </div>
-    <?php endif;
-    ?>
+<?php endif;?>
     
 </div>
